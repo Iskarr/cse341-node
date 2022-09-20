78dd97b3422fbe96684c9ba95cb1d29a5a77329d
Austin Donovan <iskarr@Austins-MacBook-Pro.local>
2022-09-19 19:46:17 -0600

    added db,routes,controllers.

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,15 @@
-const routes = require("express").Router();
+// const routes = require("express").Router();
 
-routes.get("/", (req, res) => {
-  res.send("Hello Sarah!!!");
-});
+// routes.get("/", (req, res) => {
+//   res.send("Hello Sarah!!!");
+// });
 
-module.exports = routes;
+// module.exports = routes;
+
+const express = require("express");
+const router = express.Router();
+
+router.use("/contacts", require("./contacts"));
+router.use("/contact", require("./contacts"));
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,33 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
-
+const MongoClient = require("mongodb").MongoClient;
+const mongodb = require("./db/connect");
+// const router = require("./routes");
+const contacts = require("./routes/contacts");
 app.use("/", require("./routes"));
+app.use("/contacts", contacts);
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port: ${PORT}`);
+app
+  .use(express.json())
+  .use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    next();
+  })
+  .use("/", require("./routes"));
+
+mongodb.initDb((err, mongodb) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(PORT);
+    console.log(
+      "\x1b[34m%s\x1b[0m",
+      `Connected to DB and listening on ${PORT}`
+    );
+  }
 });
+
+// app.listen(PORT, () => {
+//   console.log(`Example app listening on port: ${PORT}`);
+// });
